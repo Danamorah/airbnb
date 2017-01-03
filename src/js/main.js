b655fc8a858e7a68c0fe591da67341d12e47ef36
marcia <marcia.millanao@gmail.com>
2017-01-03 11:36:58 -0300

    actualizacion de filtros avanzados

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+	$('.carousel').carousel();
+});
 
- $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
+$(document).ready(function(){
+	$('.slider').slider({full_width: true});
+});
+
+/**inicio js filtros avanzados*/
+$(document).ready(function(){
+	$('.collapsible').collapsible();
+});
+
+$(document).ready(function(){
+	$('.btn-filtros').on("click", function(){
+		$('#filtro-anvanced').show();
+	});
+});
+
+$(document).ready(function(){
+	$('.btn-cancelar').on("click", function(){
+		$('#filtro-anvanced').hide();
+	});
+});
+/**fin js filtros avanzados*/
