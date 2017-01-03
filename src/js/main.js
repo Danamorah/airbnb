5ff882ba259832c8a0da29b804c18362ba2e15dc
Your Name <you@example.com>
2017-01-03 13:47:50 -0300

    actualizando filter

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,35 @@ $(document).ready(function(){
 	});
 });
 /**fin js filtros avanzados*/
+
+// JS YENNY
+// SELECT 
+
+ $(document).ready(function() {
+    $('select').material_select();
+ });
+
+//DATE PICKER  
+       
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+ });
+
+ //TOOLTIPS
+ $(".tooltip").hover(function(){
+    $('.tooltiptext').css("display", "block");
+    }, function(){
+ 	$('.tooltiptext').css("display", "none");
+});
+
+//RANGE PRICE 
+
+$('#flat-slider').slider({
+  orientation: 'horizontal',
+  range:       true,
+  values:      [5.000,50.000]
+});
+
+//FIN JS YENNY
+
